745bd0b38baa0db3c9e6a66ee52666cb97bbb481
Nguyen Van Thuong <thuongnva2923@gmail.com>
2024-10-31 21:34:59 +0700

    feat: create logic login and logout when reset page and add item in Gift Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from '~/pages/Home/index'
 import Redemption from '~/pages/Redemption/index'
+import Gifts from '~/pages/Gifts/index'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/redemption' element={<Redemption />} />
+        <Route path='/gifts' element={<Gifts />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/Gifts/GiftContent/GiftContent.jsx
@@ -0,0 +1,143 @@
+function GiftContent() {
+  return (
+    <>
+      <div className='onboarding_3 py-[80px]'>
+        <div className='title-wrapper text-center'>
+          <p className='shadow-text'>quà siêu kun</p>
+        </div>
+      </div>
+      <div id='item-list' className='grid grid-cols-4 gap-8 mx-[100px] mb-[115px]'>
+        <div id="item-1" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-2" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-3" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-4" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-5" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-6" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-7" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+        <div id="item-8" className='gift_card shadow'>
+          <a href="">
+            <img className="gift_card_img" src="../src/assets/tuirut_tonghop.jpg" />
+          </a>
+          <div className='gift_card_body'>
+            <p>Balo KUN dây rút</p>
+            <p>1 Thẻ Siêu Quyền Năng</p>
+            <p className='gift_rating'>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+              <span className='fa fa-star'></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default GiftContent
