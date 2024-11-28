73538a1a151a7fc8203deceb97fb280715e69440
Nguyen Van Thuong <thuongnva2923@gmail.com>
2024-11-28 16:50:16 +0700

    feat: call api to select city, district, ward and show notification modal

--- frontend/src/components/NotificationModal/SuccessLoginModal.jsx
+++ frontend/src/components/NotificationModal/SuccessLoginModal.jsx
@@ -13,8 +13,9 @@ function SuccessLoginModal({ showModal, handleClose }) {
     <div className={`show-modal ${showModal ? 'block' : 'hidden'}`}>
       <div className="modal-content logout">
         <div className="modal-form text-center px-3">
-          <h1 className="text-[#00AAEC] font-bold text-2xl mb-2">Bạn đã đăng nhập thành công !</h1>
-          <div className="form flex gap-4 p-0 justify-between my-5">
+          <i className='far fa-check-circle fa-4x text-[#7AEF78] mb-2'></i>
+          <h1 className="text-[#00AAEC] font-bold text-2xl mb-4">Bạn đã đăng nhập thành công !</h1>
+          <div className="form flex gap-4 p-0 justify-between mt-5 mb-0">
             <button onClick={successLogin} type="submit" className="onboarding_button_2 text-white bg-[#00AAEC] w-[150px] mx-auto mt-0">Xác nhận</button>
           </div>
         </div>
